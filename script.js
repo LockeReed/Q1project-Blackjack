27fb0ec6dbb1096635edb286b9fdec5f37a588e7
Locke <lockereed@gmail.com>
2018-03-13 16:30:20 -0700

    added bootstrap

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+//var url = "https://deckofcardsapi.com/api/deck/";
+//var deckID = " ";
+
+
+
+
+/*fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
   .then(function(apiData) {
     return apiData.json();
   })
   .then(function(cardDeck) {
-    console.log(cardDeck)
+    //console.log(cardDeck)
+  });*/
+
+fetch("https://deckofcardsapi.com/api/deck/z2wsc5zwfwuo/draw/?count=2")
+  .then(function(apiData) {
+    return apiData.json();
+  })
+  .then(function(draw) {
+    console.log(draw.cards[0].image);
   });
