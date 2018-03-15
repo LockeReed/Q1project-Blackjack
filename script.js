0f04c6784770fd9c5192deb52cca11777a306495
Locke <lockereed@gmail.com>
2018-03-15 11:33:42 -0700

    added css

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-//var url = "https://deckofcardsapi.com/api/deck/";
-//var deckID = " ";
+var url = "https://deckofcardsapi.com/api/deck/";
+var deckID = "";
+
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+   .then(function(apiData) {
+      return apiData.json();
+   })
+   .then(function(cardDeck) {
+      console.log(cardDeck)
+      var responce = cardDeck
+      if (responce.success === true) {
+         deckID = responce.deck_id;
+      }
+      console.log(deckID)
+   });
 
 
 
-
-/*fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-  .then(function(apiData) {
-    return apiData.json();
-  })
-  .then(function(cardDeck) {
-    //console.log(cardDeck)
-  });*/
-
-fetch("https://deckofcardsapi.com/api/deck/z2wsc5zwfwuo/draw/?count=2")
-  .then(function(apiData) {
-    return apiData.json();
-  })
-  .then(function(draw) {
-    console.log(draw.cards[0].image);
-  });
+fetch("https://deckofcardsapi.com/api/deck/3fvydrrj3wh9/draw/?count= 2")
+   .then(function(apiData) {
+      return apiData.json();
+   })
+   .then(function(draw) {
+      console.log(draw);
+   })
